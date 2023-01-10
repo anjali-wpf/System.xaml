da67f5280da0383d6f6740ac66a3b5b406219a12
Anjali Sheel <anjalisheel@microsoft.com>
2023-01-10 16:37:29 +0530

    Added sample app to test the code

---
+++ ConsoleApp3/ConsoleApp3/Program.cs
@@ -0,0 +1,36 @@
+﻿//using FluentAssertions;
+
+using System.Xaml;
+
+namespace ConsoleApp3
+{
+    public record Tree
+    {
+        public List<Apple> Fruit { get; } = new();
+    }
+
+    public record Apple
+    {
+        public int Seeds { get; init; }
+    };
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Tree objectGraph = new()
+            {
+                Fruit =
+                    {
+                        new Apple { Seeds = 3 },
+                        new Apple { Seeds = 5 },
+                    },
+            };
+
+            string xaml = XamlServices.Save(objectGraph);
+            Console.WriteLine(xaml);
+            Tree deserialized = (Tree)XamlServices.Load(new StringReader(xaml));
+            Console.WriteLine($"Fruit on deserialized tree: {deserialized.Fruit.Count}");
+        }
+    }
+}
